d78955b7bf946f39012125a89dc95ddcddeadc1e
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-06-06 00:30:26 +0200

    Add task 1 initial code to refactor

--- src/goit_pythonweb_hw_01/task_1/main.py
+++ src/goit_pythonweb_hw_01/task_1/main.py
@@ -2,9 +2,31 @@ import logging
 
 from goit_pythonweb_hw_01.utils.logging_config import setup_logging
 
+
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def start_engine(self):
+        print(f"{self.make} {self.model}: Engine started")
+
+
+class Motorcycle:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def start_engine(self):
+        print(f"{self.make} {self.model}: Engine started")
+
+
 if __name__ == "__main__":
     setup_logging()
     logger = logging.getLogger("task 1")
 
-    logger.info("Running task 1 example...")
-    # task 1 code here
+    vehicle1 = Car("Toyota", "Corolla")
+    vehicle1.start_engine()
+
+    vehicle2 = Motorcycle("Harley-Davidson", "Sportster")
+    vehicle2.start_engine()
